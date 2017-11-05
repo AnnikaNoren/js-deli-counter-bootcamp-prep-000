83fd1d6d037ad5cb5a3ba3f102374b84fa8d9b4c
Annika Noren <amenoren@gmail.com>
2017-11-05 21:07:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function takeANumber(katzDeliLine, newPerson) {
+  katzDeliLine.push(newPerson);
+  return(`Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`);
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length == 0){
+    return(`There is nobody waiting to be served!`)
+  }
+
+  return(`Currently serving ${katzDeliLine.shift()}.`)
+
+
+}
+
+function currentLine(katzDeliLine) {
+
+  while (katzDeliLine.length > 0){
+    return(`The line is currently: `);
+    for (i = 0; i >= katzDeliLine.length; i++){
+      return(` ${i}. ${katzDeliLine[i]},`)
+    }
+  }
+
+  while (katzDeliLine.length == 0){
+    return(`The line is currently empty.`)
+  }
+}
